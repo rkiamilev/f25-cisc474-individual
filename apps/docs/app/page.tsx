a329ff3ff09880e6e5fb98f38fa38f276f264585
rickykiamilev <rickyk@udel.edu>
2025-09-18 00:24:13 -0400

    branch

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -33,7 +33,13 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>apps/docs/app/page.tsx</code>
+            GEGHAEGHAELGHLAEGHIH started by editing <code>apps/docs/app/page.tsx</code>
+          </li>
+          <li>
+            GEGHAEGHAELGHLAEGHIH started by editing <code>apps/docs/app/page.tsx</code>
+          </li>
+          <li>
+            GEGHAEGHAELGHLAEGHIH started by editing <code>apps/docs/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
