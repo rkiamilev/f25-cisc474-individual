0511b7a83ffe4ba2ffca56b8daef192bbe8258ea
rickykiamilev <rickyk@udel.edu>
2025-10-08 21:49:03 -0400

    render reconigure tsconfig.json deploy main

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@repo/database';
+import { PrismaClient } from '../../../packages/database/generated/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
